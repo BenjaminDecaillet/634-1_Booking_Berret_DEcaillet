7e561a1105ed41afddea13406665f521b524897d
BenjaminDecaillet <decaillet.benjamin@gmail.com>
2017-12-15 17:27:56 +0100

    Initialisation of model 2

---
+++ BookingBerretDecaillet/BookingBerretDecaillet/HotelManagement.cs
@@ -0,0 +1,33 @@
+namespace BookingBerretDecaillet
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class HotelManagement : DbContext
+    {
+        // Your context has been configured to use a 'HotelManagement' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'BookingBerretDecaillet.HotelManagement' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'HotelManagement' 
+        // connection string in the application configuration file.
+        public HotelManagement()
+            : base("name=HotelManagement")
+        {
+        }
+
+        public System.Data.Entity.DbSet<BookingBerretDecaillet.Models.Hotel> Hotels { get; set; }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
