8b6dd5dc8bb20a18e33134fa27eb7ef04fedfcc1
BenjaminDecaillet <decaillet.benjamin@gmail.com>
2017-12-15 17:46:46 +0100

    Controllers and config prepared

--- BookingBerretDecaillet/BookingBerretDecaillet/HotelManagement.cs
+++ BookingBerretDecaillet/BookingBerretDecaillet/HotelManagement.cs
@@ -19,6 +19,10 @@ namespace BookingBerretDecaillet
 
         public System.Data.Entity.DbSet<BookingBerretDecaillet.Models.Hotel> Hotels { get; set; }
 
+        public System.Data.Entity.DbSet<BookingBerretDecaillet.Models.Room> Rooms { get; set; }
+
+        public System.Data.Entity.DbSet<BookingBerretDecaillet.Models.Picture> Pictures { get; set; }
+
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
