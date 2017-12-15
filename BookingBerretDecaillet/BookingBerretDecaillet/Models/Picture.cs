937db1f9df94c010373adbba88fb512b56980e69
BenjaminDecaillet <decaillet.benjamin@gmail.com>
2017-12-15 17:15:12 +0100

    Code for Code first

---
+++ BookingBerretDecaillet/BookingBerretDecaillet/Models/Picture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookingBerretDecaillet.Models
+{
+    public class Picture
+    {
+        [Key]
+        public int IdPicture { get; set; }
+        public string Url { get; set; }
+        public Room Room { get; set; }
+
+        public override string ToString()
+        {
+            return "idPicture : " + IdPicture +
+                "url : " + Url +
+                " room : " + Room;
+        }
+    }
+}
